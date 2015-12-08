c8789f9a8ad6d365162452b0280b5bf07c77532d
Bapu Sethi <bapu.sethi@messagexchange.com>
2015-12-08 18:39:47 +1100

    allowing string for per page

--- lib/grasshopper_paginate/active_record.rb
+++ lib/grasshopper_paginate/active_record.rb
@@ -6,7 +6,8 @@ module Grasshopper
     module ActiveRecord
       module Pagination
         def smart_paginate(options={})
-          per_page = options[:per_page] || self.per_page
+          per_page = self.per_page
+          per_page = options[:per_page].to_i if options[:per_page]
           options[:per_page] = (per_page*1.5).to_i if self.count <= per_page*1.5
           paginate options
         end
--- lib/grasshopper_paginate/array.rb
+++ lib/grasshopper_paginate/array.rb
@@ -2,7 +2,8 @@ require 'will_paginate/array'
 
 class Array
   def smart_paginate(options={})
-    per_page = options[:per_page] || WillPaginate.per_page
+    per_page = WillPaginate.per_page
+    per_page = options[:per_page].to_i if options[:per_page]
     options[:per_page] = (per_page*1.5).to_i if self.count <= per_page*1.5
     paginate options
   end
